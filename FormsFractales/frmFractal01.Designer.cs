7bb52d5cdaaa7f6337b8e590f9e7b04f0fa5a357
Mexta46 <22030043@itesa.edu.mx>
2025-03-23 14:39:47 -0600

    Agregar fractales

---
+++ FormsFractales/frmFractal01.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FormsFractales
+{
+    partial class frmFractal01
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ptbMandelbrot = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)ptbMandelbrot).BeginInit();
+            SuspendLayout();
+            // 
+            // ptbMandelbrot
+            // 
+            ptbMandelbrot.Location = new Point(105, 12);
+            ptbMandelbrot.Name = "ptbMandelbrot";
+            ptbMandelbrot.Size = new Size(550, 550);
+            ptbMandelbrot.TabIndex = 0;
+            ptbMandelbrot.TabStop = false;
+            // 
+            // frmFractal01
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 576);
+            Controls.Add(ptbMandelbrot);
+            Name = "frmFractal01";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Fractal 01";
+            Load += frmFractal01_Load;
+            ((System.ComponentModel.ISupportInitialize)ptbMandelbrot).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox ptbMandelbrot;
+    }
+}
---
+++ FormsFractales/frmFractal01.cs
@@ -0,0 +1,52 @@
+namespace FormsFractales
+{
+    public partial class frmFractal01 : Form
+    {
+        public frmFractal01()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFractal01_Load(object sender, EventArgs e)
+        {
+            MandelbrodSet();
+        }
+
+        private void MandelbrodSet()
+        {
+            int widht = ptbMandelbrot.Width;
+            int height = ptbMandelbrot.Height;
+
+            Bitmap bmp = new Bitmap(widht, height);
+            for (int row = 0; row < widht; row++) {
+                for (int col = 0; col < height; col++) {
+                    double c_re = (col - widht / 2.0) * 4.0 / widht;
+                    double c_im = (row - height / 2.0) * 4.0 / height;
+
+                    int iteraciones = 0;
+                    double x = 0, y = 0;
+
+                    while(iteraciones < 1000 && ((x * x) + (y * y)) < 4.0)
+                    {
+                        double x_temporal = (x * x) - (y * y) + c_re;
+                        y = 2 * x * y + c_im;
+                        x = x_temporal;
+                        iteraciones++;
+                    }
+
+                    if (iteraciones < 1000)
+                    {
+                        //bmp.SetPixel(col, row, Color.White);
+                        bmp.SetPixel(col, row, Color.FromArgb(iteraciones%128, iteraciones%40*5, iteraciones%10));
+                    }
+                    else 
+                    {
+                        bmp.SetPixel(col, row, Color.Black);
+                    }
+                }
+
+            }
+            ptbMandelbrot.Image = bmp;
+        }
+    }
+}
